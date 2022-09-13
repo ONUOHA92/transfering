0a0cf479ad486d09dea43a24fccd3f4fa4910ecf
Williams Alex <godswillwilliams9@gmail.com>
2022-09-13 10:48:51 +0100

    Fixed some minor issues

--- src/pages/signup/signup-as-coach-components/PhaseFour.tsx
+++ src/pages/signup/signup-as-coach-components/PhaseFour.tsx
@@ -1,51 +1,40 @@
-
-import React from "react";// @ts-ignore
+import React from "react"; // @ts-ignore
 import Uploader from "../../../components/Input/Uploader.tsx";
 // @ts-ignore
 import Button from "../../../components/Buttons/Button.tsx";
 
 import "../styles.scss";
 
-
-
-
-
 const PhaseFour = ({ handleNextStep, coach, setCoach }: any): JSX.Element => {
-
+  const handleGetChangeValue = (e: any) => {
+    console.log("Res log ===>", e.target.files[0]);
+  };
 
   const onFileChange = (e: any) => {
-    console.log(e.target.files[0])
-    setCoach({ ...coach, image: [...coach.image, e.target.files[0]] })
-  }
+    console.log(e.target.files[0]);
+    setCoach({ ...coach, image: [...coach.image, e.target.files[0]] });
+  };
 
   return (
     <div>
       <p className="reg-label mb-2">Licenses/Certificates (Image)</p>
       <div className="mt-1">
-        <Uploader
-          type='file'
-          onChange={onFileChange}
-        />
-
+        <Uploader type="file" onChange={handleGetChangeValue} />
       </div>
       <p className="reg-label mb-2 mt-8">Diploma Certificate (Image)</p>
       <div className="mt-1">
-        <Uploader
-          type='file'
-          value={coach.image}
-          onChange={onFileChange}
-        />
+        <Uploader type="file" value={coach.image} onChange={onFileChange} />
       </div>
-      <p className="reg-label mb-2 mt-8">Other Trainings & Certifications (Image)</p>
+      <p className="reg-label mb-2 mt-8">
+        Other Trainings & Certifications (Image)
+      </p>
       <div className="mt-1">
-        <Uploader
-          type='file'
-          value={coach.image}
-          onChange={onFileChange}
-        />
+        <Uploader type="file" value={coach.image} onChange={onFileChange} />
       </div>
 
-      <p className="reg-label mb-2 mt-8">Other Trainings & Certifications (Image)</p>
+      <p className="reg-label mb-2 mt-8">
+        Other Trainings & Certifications (Image)
+      </p>
       <textarea className="input w-full txt-area px-5 py-5" />
       <div className="flex justify-center py-10">
         <Button
